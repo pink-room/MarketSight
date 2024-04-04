4b7f8e3b5114fe65c89f46c716553c57a6073409
LucasPrioste <lucasprioste@hotmail.com>
2024-04-04 14:43:42 +0100

    Update Colors and Theme

--- app/src/main/java/dev/pinkroom/marketsight/ui/theme/Theme.kt
+++ app/src/main/java/dev/pinkroom/marketsight/ui/theme/Theme.kt
@@ -16,25 +16,19 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlackBackground,
+    onPrimary = White,
+    surfaceVariant = Gray,
+    surface = BlackBackground,
+    onSurfaceVariant = White,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = GrayBackground,
+    onPrimary = Black,
+    surfaceVariant = White,
+    surface = GrayBackground,
+    onSurfaceVariant = Black,
 )
 
 @Composable
@@ -57,7 +51,8 @@ fun MarketSightTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.navigationBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
