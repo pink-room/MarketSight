5cb1ae5ea4ef0f805a4915723960e3ef22f0744b
LucasPrioste <lucasprioste@hotmail.com>
2024-04-05 14:15:38 +0100

    Dimensions File

--- app/src/main/java/dev/pinkroom/marketsight/ui/theme/Type.kt
+++ app/src/main/java/dev/pinkroom/marketsight/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package dev.pinkroom.marketsight.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -20,24 +21,75 @@ val FontFamilyUbuntu = FontFamily(
 )
 
 private val defaultTypography = Typography()
-val Typography = Typography(
-    displayLarge = defaultTypography.displayLarge.copy(fontFamily = FontFamilyUbuntu),
-    displayMedium = defaultTypography.displayMedium.copy(fontFamily = FontFamilyUbuntu),
-    displaySmall = defaultTypography.displaySmall.copy(fontFamily = FontFamilyUbuntu),
-
-    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = FontFamilyUbuntu),
-    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = FontFamilyUbuntu),
-    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = FontFamilyUbuntu),
-
-    titleLarge = defaultTypography.titleLarge.copy(fontFamily = FontFamilyUbuntu),
-    titleMedium = defaultTypography.titleMedium.copy(fontFamily = FontFamilyUbuntu),
-    titleSmall = defaultTypography.titleSmall.copy(fontFamily = FontFamilyUbuntu),
-
-    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = FontFamilyUbuntu),
-    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = FontFamilyUbuntu),
-    bodySmall = defaultTypography.bodySmall.copy(fontFamily = FontFamilyUbuntu),
-
-    labelLarge = defaultTypography.labelLarge.copy(fontFamily = FontFamilyUbuntu),
-    labelMedium = defaultTypography.labelMedium.copy(fontFamily = FontFamilyUbuntu),
-    labelSmall = defaultTypography.labelSmall.copy(fontFamily = FontFamilyUbuntu)
+val Typography @Composable get() = Typography(
+    // ----- START DISPLAY -----
+    displayLarge = defaultTypography.displayLarge.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.displayLarge,
+    ),
+    displayMedium = defaultTypography.displayMedium.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.displayMedium,
+    ),
+    displaySmall = defaultTypography.displaySmall.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.displaySmall,
+    ),
+    // ----- END DISPLAY -----
+    // ----- START HEADLINE -----
+    headlineLarge = defaultTypography.headlineLarge.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.headlineLarge,
+    ),
+    headlineMedium = defaultTypography.headlineMedium.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.headlineMedium,
+    ),
+    headlineSmall = defaultTypography.headlineSmall.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.headlineSmall,
+    ),
+    // ----- END HEADLINE -----
+    // ----- START TITLE -----
+    titleLarge = defaultTypography.titleLarge.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.titleLarge,
+    ),
+    titleMedium = defaultTypography.titleMedium.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.titleMedium,
+    ),
+    titleSmall = defaultTypography.titleSmall.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.titleSmall,
+    ),
+    // ----- END TITLE -----
+    // ----- START BODY -----
+    bodyLarge = defaultTypography.bodyLarge.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.bodyLarge,
+    ),
+    bodyMedium = defaultTypography.bodyMedium.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.bodyMedium,
+    ),
+    bodySmall = defaultTypography.bodySmall.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.bodySmall,
+    ),
+    // ----- END BODY -----
+    // ----- START LABEL -----
+    labelLarge = defaultTypography.labelLarge.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.labelLarge,
+    ),
+    labelMedium = defaultTypography.labelMedium.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.labelMedium,
+    ),
+    labelSmall = defaultTypography.labelSmall.copy(
+        fontFamily = FontFamilyUbuntu,
+        fontSize = dimens.labelSmall,
+    )
+    // ----- END LABEL -----
 )
