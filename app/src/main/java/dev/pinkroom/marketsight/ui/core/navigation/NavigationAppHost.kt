388f4f22899cd7f0d7d3060dd1b5c5d5a21edf12
LucasPrioste <lucasprioste@hotmail.com>
2024-04-08 14:46:36 +0100

    route with arg symbolId on Detail screen

--- app/src/main/java/dev/pinkroom/marketsight/ui/core/navigation/NavigationAppHost.kt
+++ app/src/main/java/dev/pinkroom/marketsight/ui/core/navigation/NavigationAppHost.kt
@@ -3,8 +3,11 @@ package dev.pinkroom.marketsight.ui.core.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import dev.pinkroom.marketsight.ui.core.navigation.Args.SYMBOL_ID
 import dev.pinkroom.marketsight.ui.detail_screen.DetailScreen
 import dev.pinkroom.marketsight.ui.home_screen.HomeScreen
 import dev.pinkroom.marketsight.ui.news_screen.NewsScreen
@@ -20,13 +23,24 @@ fun NavigationAppHost(
         startDestination = startDestination.route,
         modifier = modifier
     ) {
-        composable(Route.HomeScreen.route) {
+        composable(
+            route = Route.HomeScreen.route,
+        ) {
             HomeScreen()
         }
-        composable(Route.NewsScreen.route) {
+        composable(
+            route = Route.NewsScreen.route,
+        ) {
             NewsScreen()
         }
-        composable(Route.DetailScreen.route) {
+        composable(
+            route = Route.DetailScreen.route,
+            arguments = listOf(
+                navArgument(name = SYMBOL_ID){
+                    type = NavType.StringType
+                },
+            )
+        ) {
             DetailScreen()
         }
     }
